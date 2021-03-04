3a194d4b5f6821810cda03dcafd9fba385d65fce
Bruce <broshanravan@gmail.com>
2021-03-04 11:02:35 +0000

    Adding more unit Tests

--- src/test/java/com/jupiter/car/hire/Buisness/BookingServiceTest.java
+++ src/test/java/com/jupiter/car/hire/Buisness/BookingServiceTest.java
@@ -1,4 +1,25 @@
 package com.jupiter.car.hire.Buisness;
 
+import com.jupiter.car.hire.beans.Address;
+import com.jupiter.car.hire.beans.Customer;
+import com.jupiter.car.hire.beans.Vehicle;
+import com.jupiter.car.hire.enums.CustomerType;
+import org.junit.BeforeClass;
+
 public class BookingServiceTest {
+
+
+    Vehicle van;
+    Vehicle estate;
+    Vehicle car;
+
+
+    @BeforeClass
+    public static void Setup(){
+
+
+
+    }
+
+
 }
